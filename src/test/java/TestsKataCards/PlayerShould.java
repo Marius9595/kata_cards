bf7a0faf44aa7a537c39d6ac2ee7b6960803c8bd
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2022-04-02 16:03:56 +0100

    Initial commit

---
+++ src/test/java/TestsKataCards/PlayerShould.java
@@ -0,0 +1,32 @@
+package TestsKataCards;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+/*
+  ****CARD FUNCIONALITIES****
+
+  # value of cards follows a ranking : 1,2,3,4,5,6,7,8,9,J,Q,K
+  # According to ranking, when two cards face to each other, the winner one is the best
+    in the ranking
+
+  ****PLAYER FUNCIONALITIES****
+
+  # Players will have two card to play
+  # Players cannot change the order of cards in order to show them
+  # players win when, after cards were used, has the highest score
+
+ */
+
+
+class PlayerShould {
+
+    @Test
+    void pass_happy_test(){
+        assertEquals(1,1);
+    }
+
+    //
+}
